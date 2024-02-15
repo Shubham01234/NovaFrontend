ed62ef20142780270c51e9b4801f48ff5e2aaa37
sanskarkatyare24 <sanskar.k@rejolut.com>
2024-02-15 16:54:26 +0530

    feat : Created button

---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+interface ButtonProps {
+  color?: "primary" | "secondary " | "third" | "default";
+  size?: "small" | "medium" | "large";
+  disable?: boolean;
+  label: string;
+  onClick?: () => void;
+  onSubmit?: () => void;
+  style?: object;
+  className?: string;
+  loading?: boolean;
+  type?: "submit" | "button" | "reset" | undefined;
+  variant?: "standard" | "line";
+  icon?: string | any;
+  parentBgColor: string;
+}
+
+export const Button = ({
+  color = "primary",
+  variant = "standard",
+  size = "medium",
+  disable = false,
+  label,
+  style,
+  icon,
+  className = "",
+  loading = false,
+  type = "button",
+  parentBgColor,
+  ...props
+}: ButtonProps) => {
+  const sizeValues = {
+    small: "h-7 min-w-[353px]",
+    medium: "h-8 min-w-[353px]",
+    large: "h-10 min-w-[353px]",
+  };
+
+  const colorValues = {
+    primary:
+      variant === "standard"
+        ? "bg-background-primary text-text-primary"
+        : "text-light-primary-500 hover:text-text-primary",
+    secondary:
+      variant === "standard"
+        ? "bg-background-secondary text-dark-primary-500"
+        : "text-light-gray-300 hover:text-text-primary",
+    default:
+      variant === "standard"
+        ? "bg-background-default text-text-primary"
+        : "text-text-primary hover:text-text-primary",
+    third:
+      variant === "standard"
+        ? "bg-background-third text-text-primary"
+        : "text-text-primary hover:bg-light-primary-default",
+  };
+
+  const variantValues = {
+    standard: `justify-center p-2 px-3 border border-transparent
+    ${
+      disable || loading
+        ? "bg-light-gray-25 text-light-gray-75"
+        : colorValues[color] +
+          `${
+            color === "default"
+              ? " hover:bg-danger-500 hover:text-white"
+              : " hover:bg-light-primary-hover hover:text-white"
+          }`
+    } ${sizeValues[size]}`,
+    line: `${
+      disable || loading ? "text-dark-secondary-300" : colorValues[color]
+    } h-8 min-w-max justify-start`,
+  };
+
+  const additionalStyles = ` before:content-[''] before:absolute before:left-0 before:top-0 before:w-0 before:h-0 before:border-t-[20px] before:border-t-[${parentBgColor}] before:border-r-transparent before:border-r-[20px] after:content-['']  after:absolute after:right-0 after:bottom-0 after:w-0 after:h-0 after:border-t-[20px] after:border-t-transparent after:border-r-[20px] after:border-r-[${parentBgColor}]`;
+
+  return (
+    <button
+      type={type}
+      className={`flex items-center relative text-sm ${
+        variantValues[variant]
+      } ${sizeValues[size]} ${
+        disable || loading ? "cursor-not-allowed" : "cursor-pointer"
+      } ${icon ? "gap-1" : ""} ${className} ${additionalStyles} `}
+      disabled={loading ? true : disable}
+      style={{ ...style }}
+      {...props}
+    >
+      {loading ? (
+        <div className="flex min-w-min justify-center gap-2">
+          <div
+            className={`h-6 w-6 animate-spin border ${
+              disable ? "border-white" : "border-gray-600"
+            }`}
+          ></div>
+          loading...
+        </div>
+      ) : (
+        <>
+          {icon && <span>{icon}</span>}
+          {label}
+        </>
+      )}
+      <div
+        className="absolute left-0 top-0 w-0 h-0 border-t-20 border-b-0 border-r-20 border-transparent"
+        style={{ borderColor: "#f06d06" }}
+      ></div>
+      <div
+        className="absolute bottom-0 right-0 w-0 h-0 border-t-20 border-b-0 border-r-20 border-transparent"
+        style={{ borderColor: "#f06d06" }}
+      ></div>
+      
+    </button>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
+      fontSize: {
+        xxs: "10px",
+        xs: "12px",
+        small: "14px",
+        medium: "16px",
+        large: "18px",
+        xl: "20px",
+        xxl: "24px",
+        xxxl: "30px",
+        xxxxl: "36px",
+        xxxxxl: "42px",
+        xxxxxxl: "48px",
+        xxxxxxxl: "54px",
+      },
+      colors: {
+        text: {
+          primary: "#FAFAFA",
+          secondary: "#1CF9CF",
+        },
+        background: {
+          primary: "#000000",
+          secondary: "#5765F2",
+          third: "#08C4B3",
+          default: "#1E49E2",
+        },
+      },
+      borderWidth: {
+        xs: "1px",
+        small: "2px",
+        medium: "3px",
+        large: "4px",
+      },
+      borderRadius: {
+        xs: "2px",
+        small: "4px",
+        medium: "6px",
+        large: "8px",
+        xl: "10px",
+        xxl: "12px",
+        xxxl: "14px",
+        xxxxl: "16px",
+        xxxxxl: "18px",
+        xxxxxxl: "20px",
+        full: "9999px",
+      },
+      boxShadow: {
+        "primary-button": "0px 4px 12px 0px rgba(89, 37, 254, 0.25)",
+        E1: "1px 1px 5px 0px rgba(0, 0, 0, 0.14)",
+        E2: "0px 4px 8px 2px rgba(0, 0, 0, 0.14)",
+        E3: "0px 4px 5px -1px rgba(0, 0, 0, 0.08)",
+        E4: "0px 6px 7px -2px rgba(0, 0, 0, 0.08)",
+        E5: "0px 8px 11px -3px rgba(0, 0, 0, 0.08)",
+        E6: "0px 12px 19px -5px rgba(0, 0, 0, 0.08)",
+        E7: "0px 0px 25px -7px rgba(0, 0, 0, 0.16)",
+        E8: "0px 24px 33px -9px rgba(0, 0, 0, 0.1)",
+        E9: "0px 40px 55px -11px rgba(0, 0, 0, 0.12)",
+        "onboarding-image": "0px 0px 20px 0px rgba(0, 0, 0, 0.2)",
+      },
+      maxHeight: {
+        wrapper: "calc(100vh - 80px)",
+      },
+      minWidth: {
+        247: "247px",
+      },
+    },
+    plugins: [],
+  },
+};
