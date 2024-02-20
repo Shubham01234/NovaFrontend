6876e32d8abe9851efbabb6293d9e3a86102d895
sanskarkatyare24 <sanskar.k@rejolut.com>
2024-02-20 12:36:56 +0530

    feat : verify page ui

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,6 +2,7 @@ import LandingPage from "pages/LandingPage";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import '../../../src/App.css';
 import { SignUp } from "pages/Authentication/SignUp";
+import { Verify } from "pages/Authentication/Verify";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/landing" element={<LandingPage/>}/>
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify" element={<Verify />} />
       </Routes>
     </Router>
     </>
